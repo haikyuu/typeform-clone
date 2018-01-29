d9e707a367c3ed65c8964d1e82333b7ff6212ff3
Abdellah Alaoui <als.abdellah@gmail.com>
2018-01-29 11:35:38 +0000

    [questions] fix: implement change current question as a goTo instead of goToNext

--- src/state/questions.js
+++ src/state/questions.js
@@ -10,20 +10,36 @@ export const changeCurrentQuestion = id => ({
   type: "QUESTIONS/CHANGE_CURRENT_QUESTION",
   id
 })
-
-// Reducer
+export const saveQuestionReply = (id, reply) => ({
+  type: "QUESTIONS/SAVE_QUESTION_REPLY",
+  id,
+  reply
+})
+// State shape & Initial state
 const initialState = {
   byId: entities.questions,
   all: result.questions,
-  currentQuestion: data.questions[0].id
+  currentQuestion: data.questions[0].id,
+  firstQuestion: data.questions[0].id
 }
+
+// Reducer
 const questions = (state = initialState, action) => {
   switch (action.type) {
     case "QUESTIONS/CHANGE_CURRENT_QUESTION":
-      const currentQuestion = state.byId[state.currentQuestion]
       return {
         ...state,
-        currentQuestion: currentQuestion ? currentQuestion.next : null
+        currentQuestion: action.id
+      }
+    case "QUESTIONS/SAVE_QUESTION_REPLY":
+      const { reply, id } = action
+      const question = state.byId[id]
+      return {
+        ...state,
+        byId: {
+          ...state.byId,
+          [id]: { ...question, reply }
+        }
       }
     default:
       return state
