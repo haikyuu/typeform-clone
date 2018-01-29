9f8ead982526a223c2d0ae4223466594e956b08e
Abdellah Alaoui <als.abdellah@gmail.com>
2018-01-29 14:15:06 +0000

    [submission] move question prop-type to helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,6 +1,7 @@
 import createHistory from "history/createHashHistory"
+import PropTypes from "prop-types"
 
-const history = createHistory()
+export const history = createHistory()
 
 const type = val =>
   val === null
@@ -12,7 +13,7 @@ const type = val =>
           .slice(8, -1)
           .toLowerCase()
 
-const isOfType = (rawValue, expectedType) => {
+export const isOfType = (rawValue, expectedType) => {
   let val = rawValue
   let res = type(rawValue) === expectedType
 
@@ -30,5 +31,15 @@ const isOfType = (rawValue, expectedType) => {
     convertedValue: val
   }
 }
-
-export { history, isOfType }
+export const questionPropType = PropTypes.shape({
+  id: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  reply: PropTypes.oneOfType([
+    PropTypes.string, // no reply || string
+    PropTypes.instanceOf(Date),
+    PropTypes.number,
+    PropTypes.bool
+  ]).isRequired,
+  type: PropTypes.oneOf(["string", "boolean", "date", "number"]).isRequired,
+  next: PropTypes.string
+})
